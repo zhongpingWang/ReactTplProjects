83c2d1161fb49f1e6e648a407e31d20c9fc95309
wangzp-a <123qwe!@#>
2017-04-20 13:42:09 +0800

    删除文件

--- redux/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux' 
-import AppReducer from './AppReducer' 
-import {  routerReducer } from 'react-router-redux'
-
-export default combineReducers({ 
-	routing: routerReducer,
-    AppReducer
-})
--- reduxConfig/configureStore.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'production') {
-  module.exports = require('./configureStore.prod');
-} else {
-  module.exports = require('./configureStore.dev');
-}
